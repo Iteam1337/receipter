8619e5de69385f33d3b70ba5977340bee214a0c0
Rickard Laurin <rickard.laurin@gmail.com>
2013-10-19 20:56:38 +0200

    added toggle to receipts

--- script/controllers/homecontroller.js
+++ script/controllers/homecontroller.js
@@ -4,10 +4,14 @@ angular.module('receipter').controller('HomeController', function($scope) {
   'use strict';
 
   $scope.select = function(receipt) {
-    $scope.data.receipts = $scope.data.receipts.map(function(r) {
-      r.selected = r.id === receipt.id;
-      return r;
-    });
+    if (receipt.selected) {
+      receipt.selected = false;
+    } else {
+      $scope.data.receipts = $scope.data.receipts.map(function(r) {
+        r.selected = r.id === receipt.id;
+        return r;
+      });
+    }
   };
 
 });
