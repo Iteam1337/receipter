e309f1fedd718699bf074a6aa2efd5558819cd83
Johan Öbrink <johan.obrink@gmail.com>
2013-10-19 13:56:19 +0200

    Added tests to upload flow

--- script/controllers/uploadcontroller.js
+++ script/controllers/uploadcontroller.js
@@ -1,29 +1,27 @@
 
 angular.module('receipter').controller('UploadController', function($scope, notification, camera) {
   'use strict';
-  //data:image/jpeg;base64,{{imagedata}} 
 
-  $scope.imagesource = 'img/shutter.png';
+  $scope.receipt = {
+    image: 'img/shutter.png'
+  };
+
+  var success = function(data) {
+    $scope.receipt.image = 'data:image/jpeg;base64,' + data;
+    camera.cleanup();
+  };
+
+  var fail = function(info) {
+    notification.alert(info, function() {}, 'Something went wrong', 'Oh darn!');
+    camera.cleanup();
+  };
 
   $scope.newPicture = function() {
-    camera.getPicture(
-      function(data) {
-        $scope.imagesource = 'data:image/jpeg;base64,' + data;
-        camera.cleanup();
-      },
-      function() {});
+    camera.getPicture(success, fail);
   };
 
   $scope.pictureFromLibrary = function() {
-    camera.getPicture(
-      function(data) {
-        $scope.imagesource = 'data:image/jpeg;base64,' + data;
-        camera.cleanup();
-      },
-      function() {},
-      {
-        sourceType: camera.PictureSourceType.SAVEDPHOTOALBUM
-      });
+    camera.getPicture(success, fail, { sourceType: camera.PictureSourceType.SAVEDPHOTOALBUM });
   };
 
 });
--- test/spec/controllers/uploadcontroller-tests.js
+++ test/spec/controllers/uploadcontroller-tests.js
@@ -1,20 +1,126 @@
 
 describe('UploadController', function() {
 
-  var controller, scope;
+  var controller, scope, notification, camera;
 
   beforeEach(function() {
+
+    camera = {
+      getPicture: sinon.stub(),
+      PictureSourceType: {
+        SAVEDPHOTOALBUM: 1
+      },
+      cleanup: sinon.spy()
+    };
+
+    notification = {
+      alert: sinon.spy()
+    };
+
     module('receipter');
     inject(function($controller, $rootScope) {
       scope = $rootScope.$new();
       controller = $controller('UploadController', {
-        $scope: scope
+        $scope: scope,
+        notification: notification,
+        camera: camera
       });
     });
   });
 
   it('is registered correctly', function() {
     expect(controller).to.be.an('Object');
+  });
+
+  describe('new receipt', function() {
+    it('creates an empty receipt object if none exists', function() {
+      expect(scope.receipt).to.be.an('Object');
+    });
+    it('sets image to img/shutter.png as default', function() {
+      expect(scope.receipt.image).to.equal('img/shutter.png');
+    });
+
+    describe('picture', function() {
+
+      describe('new picture', function() {
+
+        describe('success', function() {
+          it('calls getPicture on camera with default parameters', function() {
+            scope.newPicture();
+            expect(camera.getPicture).to.have.been.calledOnce;
+            expect(camera.getPicture.getCall(0).args).to.have.length(2);
+            expect(camera.getPicture.getCall(0).args[0]).to.be.a('function');
+            expect(camera.getPicture.getCall(0).args[1]).to.be.a('function');
+          });
+          it('sets the result as an image on receipt', function() {
+            scope.newPicture();
+            camera.getPicture.callArgWith(0, 'imagedata');
+            expect(scope.receipt.image).to.equal('data:image/jpeg;base64,imagedata');
+          });
+          it('cleans up afterwards', function() {
+            scope.newPicture();
+            camera.getPicture.callArgWith(0, 'imagedata');
+            expect(camera.cleanup).to.have.been.calledOnce;
+          });
+        });
+
+        describe('fail', function() {
+          it('displays an error message if something goes wrong', function() {
+            scope.newPicture();
+            camera.getPicture.callArgWith(1, 'It borked!');
+            expect(notification.alert).to.have.been.calledOnce;
+            expect(notification.alert.getCall(0).args[0]).to.equal('It borked!');
+            expect(notification.alert.getCall(0).args[1]).to.be.a('function');
+            expect(notification.alert.getCall(0).args[2]).to.equal('Something went wrong');
+            expect(notification.alert.getCall(0).args[3]).to.equal('Oh darn!');
+          });
+          it('cleans up afterwards', function() {
+            scope.newPicture();
+            camera.getPicture.callArgWith(1, 'It borked!');
+            expect(camera.cleanup).to.have.been.calledOnce;
+          });
+        });
+      });
+      describe('picture from library', function() {
+        describe('success', function() {
+          it('calls getPicture on camera with correct sourceType parameter', function() {
+            scope.pictureFromLibrary();
+            expect(camera.getPicture).to.have.been.calledOnce;
+            expect(camera.getPicture.getCall(0).args).to.have.length(3);
+            expect(camera.getPicture.getCall(0).args[0]).to.be.a('function');
+            expect(camera.getPicture.getCall(0).args[1]).to.be.a('function');
+            expect(camera.getPicture.getCall(0).args[2]).to.eql({ sourceType: camera.PictureSourceType.SAVEDPHOTOALBUM });
+          });
+          it('sets the result as an image on receipt', function() {
+            scope.pictureFromLibrary();
+            camera.getPicture.callArgWith(0, 'imagedata');
+            expect(scope.receipt.image).to.equal('data:image/jpeg;base64,imagedata');
+          });
+          it('cleans up afterwards', function() {
+            scope.pictureFromLibrary();
+            camera.getPicture.callArgWith(0, 'imagedata');
+            expect(camera.cleanup).to.have.been.calledOnce;
+          });
+        });
+
+        describe('fail', function() {
+          it('displays an error message if something goes wrong', function() {
+            scope.pictureFromLibrary();
+            camera.getPicture.callArgWith(1, 'It borked!');
+            expect(notification.alert).to.have.been.calledOnce;
+            expect(notification.alert.getCall(0).args[0]).to.equal('It borked!');
+            expect(notification.alert.getCall(0).args[1]).to.be.a('function');
+            expect(notification.alert.getCall(0).args[2]).to.equal('Something went wrong');
+            expect(notification.alert.getCall(0).args[3]).to.equal('Oh darn!');
+          });
+          it('cleans up afterwards', function() {
+            scope.pictureFromLibrary();
+            camera.getPicture.callArgWith(1, 'It borked!');
+            expect(camera.cleanup).to.have.been.calledOnce;
+          });
+        });
+      });
+    });
   });
 
 });
