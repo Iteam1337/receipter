8e322e2aeaad99d87f12acdc189553041c29ef5b
Johan Öbrink <johan.obrink@gmail.com>
2013-10-19 14:18:26 +0200

    Built flow

--- script/controllers/uploadcontroller.js
+++ script/controllers/uploadcontroller.js
@@ -1,10 +1,12 @@
 
-angular.module('receipter').controller('UploadController', function($scope, notification, camera) {
+angular.module('receipter').controller('UploadController', function($scope, notification, camera, $location) {
   'use strict';
 
   $scope.receipt = {
     image: 'img/shutter.png'
   };
+
+  $scope.step = 'picture';
 
   var success = function(data) {
     $scope.receipt.image = 'data:image/jpeg;base64,' + data;
@@ -22,6 +24,19 @@ angular.module('receipter').controller('UploadController', function($scope, noti
 
   $scope.pictureFromLibrary = function() {
     camera.getPicture(success, fail, { sourceType: camera.PictureSourceType.SAVEDPHOTOALBUM });
+  };
+
+  $scope.gotoStep = function(step) {
+    $scope.step = step;
+  };
+
+  $scope.upload = function() {
+    $scope.receipt.date = new Date();
+    $scope.data.receipts.push($scope.receipt);
+    $scope.receipt = {
+      image: 'img/shutter.png'
+    };
+    $location.path('/');
   };
 
 });
